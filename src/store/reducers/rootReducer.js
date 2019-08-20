021cc296a49c8cfeba6064261a3f7110c649f809
codeKing <mukamimuchiri822@gmail.com>
2019-08-20 10:42:43 +0300

    configured reducers and setup provider

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+import authReducer from './authReducer';
+import productReducer from './productReducer';
+
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  product: productReducer
+})
+
+export default rootReducer;
